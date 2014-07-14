44560179816a69e4c0f9b75339113c03e027250b
Hugo Fonseca <hdtfonseca@gmail.com>
2014-07-14 18:18:12 +0100

    Update default.rb

--- attributes/default.rb
+++ attributes/default.rb
@@ -5,4 +5,11 @@ default[:firefox]["3"] = {
  :firefox_dir => 'firefox'
 }
 
+default[:firefox]["24.6.0"] = {
+ :url => "https://ftp.mozilla.org/pub/mozilla.org/firefox/releases/24.6.0esr/linux-x86_64/pt-PT/firefox-24.6.0esr.tar.bz2",
+ :filename => "firefox-24.6.0esr.tar.bz2",
+ :sha => "35e9d570a80c587f5f302cbfa8fbfeba84883acdbe1f5d7cf1776c2453c1c18d9e82fda79f50e130485aa4e5c710779e410529f8cd3b7ffd49f6cdc2b13bf4b7",
+ :firefox_dir => 'firefox'
+}
+
 default[:firefox][:versions] = []
