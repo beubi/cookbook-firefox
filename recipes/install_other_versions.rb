d91dc6dd4efac0df76d4a762e543450ed99d56d4
Hugo Fonseca <hdtfonseca@gmail.com>
2014-07-15 14:14:01 +0100

    Update install_other_versions.rb

--- recipes/install_other_versions.rb
+++ recipes/install_other_versions.rb
@@ -7,47 +7,59 @@
 # All rights reserved - Do Not Redistribute
 #
 
-execute "apt-get update > /dev/null" do
-  action :run
-end
 
-if RUBY_PLATFORM.include? "x86_64"
-  package "ia32-libs"
-  package "libasound2"
-  package "libxrender1"
-end
 
-dir_path = "/home/vagrant/firefoxes"
+if platform?('ubuntu')
+  
+  package "firefox"
+  
+#  execute "apt-get update > /dev/null" do
+#    action :run
+#  end
 
-directory dir_path do
-  mode "0755"
-  action :create
-end
+#  if node.platform_version.to_f == 14.04
+#    if RUBY_PLATFORM.include? "x86_64"
+#    end
+#  elsif node.platform_version.to_f == 12.04
+#    if RUBY_PLATFORM.include? "x86_64"
+#      package "ia32-libs"
+#      package "libasound2"
+#      package "libxrender1"
+#    end
+#  end
 
-node[:firefox][:versions].each do |firefox_version|
-  directory "#{dir_path}/#{firefox_version}" do
+  dir_path = "/home/vagrant/firefoxes"
+
+  directory dir_path do
     mode "0755"
     action :create
   end
 
-  firefox = node[:firefox][firefox_version]
-  firefox_version_path = "#{dir_path}/#{firefox_version}"
-  tar_file = "#{firefox_version_path}/#{firefox[:filename]}"
+  node[:firefox][:versions].each do |firefox_version|
+    directory "#{dir_path}/#{firefox_version}" do
+      mode "0755"
+      action :create
+    end
 
-  remote_file tar_file do
-    source firefox[:url]
-    checksum firefox[:sha]
-    action :create_if_missing
-  end
+    firefox = node[:firefox][firefox_version]
+    firefox_version_path = "#{dir_path}/#{firefox_version}"
+    tar_file = "#{firefox_version_path}/#{firefox[:filename]}"
 
-  bash "untar firefox-#{firefox_version} to #{dir_path}" do
-    cwd firefox_version_path
-    code <<-EOH
-      tar jxf #{tar_file}
-    EOH
-  end
+    remote_file tar_file do
+      source firefox[:url]
+      checksum firefox[:sha]
+      action :create_if_missing
+    end
 
-  link "/usr/bin/firefox" do
-    to "#{firefox_version_path}/#{firefox[:firefox_dir]}/firefox"
+    bash "untar firefox-#{firefox_version} to #{dir_path}" do
+      cwd firefox_version_path
+      code <<-EOH
+        tar jxf #{tar_file}
+      EOH
+    end
+
+    link "/usr/bin/firefox" do
+      to "#{firefox_version_path}/#{firefox[:firefox_dir]}/firefox"
+    end
   end
 end
